66f8409e6e2563f3c8de28149731416fc3d382c8
artikgak <artem.hak@ukma.edu.ua>
2020-04-04 00:53:04 +0300

    add views

---
+++ CSharp_05_Hak/Tools/BaseViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
+
+namespace CSharp_05_Hak.Tools
+{
+    internal abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        internal virtual void OnPropertyChanged([CallerMemberName] string properyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(properyName));
+        }
+        #endregion
+    }
+}
